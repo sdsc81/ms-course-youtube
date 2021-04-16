1ed7366c865f471409bc421d910aee316e118cea
Santiago Da Silva <sdasilva@altimetrik.com>
2021-04-16 16:43:27 -0300

    created config server

---
+++ config-server/src/test/java/uy/com/santiagodasilva/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.santiagodasilva.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
